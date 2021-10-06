24270eed3cbe42f3d579390c793f52cafd5141d3
P-Chan <hello@0x50.io>
2021-10-07 02:44:13 +0900

    test: add test

---
+++ index.test.js
@@ -0,0 +1,32 @@
+const postcss = require('postcss')
+const postcssrc = require('postcss-load-config')
+
+let rc
+
+beforeAll(async () => {
+  rc = await postcssrc({ from: undefined })
+})
+
+test('CSS', async () => {
+  const result = await postcss(rc.plugins).process('html { background-color: black; }', rc.options)
+
+  expect(result.css).toBe('html { background-color: black; }')
+})
+
+test('Custom Properties', async () => {
+  const result = await postcss(rc.plugins).process(
+    ':root { --primary-color: black; } html { background-color: var(--primary-color); }',
+    rc.options
+  )
+
+  expect(result.css).toBe(':root { --primary-color: black; } html { background-color: var(--primary-color); }')
+})
+
+test('Custom Media Queries', async () => {
+  const result = await postcss(rc.plugins).process(
+    '@custom-media --media-query-phone (max-width: 428px); @media (--media-query-phone) { html { background-color: black; } }',
+    rc.options
+  )
+
+  expect(result.css).toBe('@media (max-width: 428px) { html { background-color: black; } }')
+})
